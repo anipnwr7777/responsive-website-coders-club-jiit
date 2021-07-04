fd4216523f526a7eebd003365abc7648fe7bc1a5
ANIRUDH PANWAR <anirudhpanwar7777@gmail.com>
2021-07-04 20:07:18 +0530

    Add files via upload

---
+++ js/resp.js
@@ -0,0 +1,39 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+rightNav = document.querySelector('.rightNav');
+navList = document.querySelector('.nav-list');
+
+burger.addEventListener('click', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+hitbtn1 = document.querySelector('.hitbtn1');
+hitbtn2 = document.querySelector('.hitbtn2');
+hitbtn3 = document.querySelector('.hitbtn3');
+hitbtn4 = document.querySelector('.hitbtn4');
+
+hitbtn1.addEventListener('click', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+hitbtn2.addEventListener('click', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+hitbtn3.addEventListener('click', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+hitbtn4.addEventListener('click', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
